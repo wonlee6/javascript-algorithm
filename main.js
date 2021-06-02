32e0e55fe018c3d3e863ffcc5014650ed1c76df3
sangwon <wonlee6@gmail.com>
2021-06-02 15:42:42 +0900

    section01

---
+++ main.js
@@ -0,0 +1,172 @@
+// 1. 세수중 최솟값
+// 2. 삼각형 판단하기
+// 3. 연필 개수
+// 4. 1부터 N까지의 합계
+// 5. 홀수
+// 6. 10부제
+// 7. 일곱난쟁이
+// 7. A to #
+
+function solution(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr = [6, 5, 11];
+console.log(solution(arr));
+
+function solution2(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr2 = [6, 7, 11];
+console.log(solution2(arr2));
+
+function solution3(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr3 = 25;
+console.log(solution3(arr3));
+
+function solution4(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr4 = 6;
+console.log(solution4(arr4));
+
+function solution5(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr5 = [5, 3, 7, 11, 2, 15, 17];
+console.log(solution5(arr5));
+
+// 합계, 홀수중 최솟값
+function solution6(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr6 = [12, 77, 38, 41, 53, 92, 85];
+console.log(solution6(arr6));
+
+function solution7(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr7 = [25, 23, 11, 47, 53, 17, 33];
+let day = 3;
+console.log(solution7(arr7, day));
+
+function solution8(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr8 = [20, 7, 23, 19, 10, 15, 8, 25, 13];
+console.log(solution8(arr8));
+
+function solution9(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr9 = 'BANANA';
+console.log(solution9(arr9));
+
+function solution10(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr10 = 'COMPUTERPROGRAMMING';
+let word = 'R';
+console.log(solution10(arr10, word));
+
+function solution11(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr11 = 'KoreaTimeGood';
+console.log(solution11(arr11));
+
+function solution12(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr12 = 'itisTimeToStudy';
+console.log(solution12(arr12));
+
+function solution13(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr13 = 'stuDY';
+console.log(solution13(arr13));
+
+function solution14(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr14 = ['time', 'tosomeplace', 'sad', 'student', 'beautiful'];
+console.log(solution14(arr14));
+
+function solution15(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr15 = 'study';
+console.log(solution15(arr15));
+
+function solution16(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr16 = 'ksekkset';
+console.log(solution16(arr16));
+
+function solution17(arr) {
+  let answer;
+
+  return answer;
+}
+
+let arr17 = [
+  'time',
+  'tosomeplace',
+  'time',
+  'student',
+  'beautiful',
+  'good',
+  'student',
+];
+console.log(solution17(arr17));
