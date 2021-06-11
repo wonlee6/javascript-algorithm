3b27140c9989b634726dd0215ec64bb3be58c5ea
wonlee6 <wonlee6@gmail.com>
2021-06-11 19:17:10 +0900

    main

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ function solution(a, b, c) {
 }
 
 console.log(solution(6, 5, 11));
+
+
 // 2. 삼각형 판단하기
+// 가장 큰 수 > 나머지 두 수의 합
+// yes or no 출력
 function solution2(a, b, c) {
   let answer;
 
@@ -14,7 +18,9 @@ function solution2(a, b, c) {
 }
 
 console.log(solution2(6, 7, 11));
+
 // 3. 연필 개수
+// 1다스에 12개 
 function solution3(arr) {
   let answer;
 
@@ -23,6 +29,7 @@ function solution3(arr) {
 
 let arr3 = 25;
 console.log(solution3(arr3));
+// 출력 3
 
 // 4. 1부터 N까지의 합계
 function solution4(arr) {
@@ -33,6 +40,8 @@ function solution4(arr) {
 
 let arr4 = 6;
 console.log(solution4(arr4));
+
+
 // 5. 최솟값 구하기
 function solution5(arr) {
   let answer;
@@ -41,7 +50,8 @@ function solution5(arr) {
 }
 
 let arr5 = [5, 3, 7, 11, 2, 15, 17];
-console.log(solution5(arr5));
+console.log(solution5(arr5)); // 2
+
 
 // 6. 홀수
 // 합계, 홀수중 최솟값
@@ -52,8 +62,11 @@ function solution6(arr) {
 }
 
 let arr6 = [12, 77, 38, 41, 53, 92, 85];
-console.log(solution6(arr6));
+console.log(solution6(arr6)); // [256,41]
+
+
 // 7. 10부제
+// 일의 자리 숫자를 보고 10부제 위반하는 차량의 대수 출력
 function solution7(arr) {
   let answer;
 
@@ -62,19 +75,22 @@ function solution7(arr) {
 
 let arr7 = [25, 23, 11, 47, 53, 17, 33];
 let day = 3;
-console.log(solution7(arr7, day));
+console.log(solution7(arr7, day)); // 3
 
+
+// 8. 일곱난쟁이
+// 일곱난쟁이의 합은 100 
+// 아홉난쟁이 중 일곱난쟁이를 구하라
 function solution8(arr) {
   let answer;
 
   return answer;
 }
-// 8. 일곱난쟁이
+
 let arr8 = [20, 7, 23, 19, 10, 15, 8, 25, 13];
-console.log(solution8(arr8));
+console.log(solution8(arr8)); // [20,7,23,19,10,8,13]
 
 // 9. A to #
-
 function solution9(arr) {
   let answer;
 
@@ -82,8 +98,10 @@ function solution9(arr) {
 }
 
 let arr9 = 'BANANA';
-console.log(solution9(arr9));
+console.log(solution9(arr9)); // B#N#N#
+
 // 10. 문자 찾기
+// R의 개수를 찾아라
 function solution10(arr) {
   let answer;
 
@@ -92,7 +110,9 @@ function solution10(arr) {
 
 let arr10 = 'COMPUTERPROGRAMMING';
 let word = 'R';
-console.log(solution10(arr10, word));
+console.log(solution10(arr10, word)); // 3
+
+
 // 11. 대문자 찾기
 function solution11(arr) {
   let answer;
@@ -101,7 +121,10 @@ function solution11(arr) {
 }
 
 let arr11 = 'KoreaTimeGood';
-console.log(solution11(arr11));
+console.log(solution11(arr11)); // 3
+
+
+
 // 12. 대문자 통일
 function solution12(arr) {
   let answer;
@@ -111,7 +134,10 @@ function solution12(arr) {
 
 let arr12 = 'itisTimeToStudy';
 console.log(solution12(arr12));
+
+
 // 13. 대소문자변환
+// 대문자는 소문자, 소문자는 대문자로 변환
 function solution13(arr) {
   let answer;
 
@@ -120,6 +146,8 @@ function solution13(arr) {
 
 let arr13 = 'stuDY';
 console.log(solution13(arr13));
+
+
 // 14. 가장 긴 문자열
 function solution14(arr) {
   let answer;
@@ -128,8 +156,11 @@ function solution14(arr) {
 }
 
 let arr14 = ['time', 'tosomeplace', 'sad', 'student', 'beautiful'];
-console.log(solution14(arr14));
+console.log(solution14(arr14)); // tosomeplace
+
+
 // 15. 가운데 문자 출력(substring, substr)
+// 짝수일땐 가운데2개출력
 function solution15(arr) {
   let answer;
 
@@ -137,7 +168,9 @@ function solution15(arr) {
 }
 
 let arr15 = 'study';
-console.log(solution15(arr15));
+console.log(solution15(arr15)); // u
+
+
 // 16. 중복문자제거(indexof)
 function solution16(arr) {
   let answer;
@@ -146,7 +179,9 @@ function solution16(arr) {
 }
 
 let arr16 = 'ksekkset';
-console.log(solution16(arr16));
+console.log(solution16(arr16)); // kset
+
+
 // 17. 중복단어제거
 function solution17(arr) {
   let answer;
